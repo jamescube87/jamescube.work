841749560061a55a9bc63ec12241c31694d9e0b4
James Jeong <jamescube87@gmail.com>
2025-07-28 23:56:47 +0900

    버전 3.8.1로 갱신

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,6 +9,11 @@ const config: Config = {
   tagline: '제임스큐브의 작업물 모음',
   favicon: 'img/favicon.ico',
 
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
   // Set the production url of your site here
   url: 'https://jamescube.work',
   // Set the /<baseUrl>/ pathname under which your site is served
